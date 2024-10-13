1782f7d9651b498b6e7b2aba93f2eccd97490d04
Toph <revertzlol@gmail.com>
2024-10-13 14:46:25 -0400

    Added code  so that the user can enter the number of the number of customers that were surveyed

--- Energy Drink Consumption.cpp
+++ Energy Drink Consumption.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 
 int main() {
-    // Constants
-    const int TOTAL_CUSTOMERS_SURVEYED = 16500;
+    // Variables for user input
+    int totalCustomersSurveyed;
     const double ENERGY_DRINK_PERCENTAGE = 0.15; // 15% of customers purchase energy drinks
     const double CITRUS_PREFERENCE_PERCENTAGE = 0.58; // 58% prefer citrus flavor
 
+    // Get input from user
+    cout << "Enter the total number of customers surveyed: ";
+    cin >> totalCustomersSurveyed;
+
     // Calculate the number of customers who purchase energy drinks
-    int energyDrinkCustomers = TOTAL_CUSTOMERS_SURVEYED * ENERGY_DRINK_PERCENTAGE;
+    int energyDrinkCustomers = totalCustomersSurveyed * ENERGY_DRINK_PERCENTAGE;
 
     // Calculate the number of customers who prefer citrus flavor
     int citrusPreferringCustomers = energyDrinkCustomers * CITRUS_PREFERENCE_PERCENTAGE;
